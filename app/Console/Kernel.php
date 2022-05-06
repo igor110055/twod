91f8954883a0340f1b75aaa2b37f5f952fa13105
DESKTOP-2K6F7VH\Ko Soe <soelay211108@gmail.com>
2022-05-06 14:11:58 +0630

    fixed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
      * Register the commands for the application.
      *
      * @return void
-     */
+     *///
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
