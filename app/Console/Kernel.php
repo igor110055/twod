34717dcbc4a5da89a169fddb6c5ca55405c7d838
DESKTOP-2K6F7VH\Ko Soe <soelay211108@gmail.com>
2022-05-18 00:22:26 +0630

    runInBackground

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,11 +30,11 @@ class Kernel extends ConsoleKernel
         date_default_timezone_set("Asia/Yangon");
         //$schedule->command('twod:cron')->everyMinute()->runInBackground();
         // $schedule->command('inspire')->hourly();
-        $schedule->command('twod:cron')->timezone('Asia/Yangon')->at("12:16");
-        $schedule->command('one:cron')->timezone('Asia/Yangon')->at("12:17");
-        $schedule->command('two:cron')->timezone('Asia/Yangon')->at("14:18");
-        $schedule->command('three:cron')->timezone('Asia/Yangon')->at("16:19");
-        $schedule->command('four:cron')->timezone('Asia/Yangon')->at("18:20");
+        $schedule->command('twod:cron')->timezone('Asia/Yangon')->runInBackground()->at("12:16");
+        $schedule->command('one:cron')->timezone('Asia/Yangon')->runInBackground()->at("12:17");
+        $schedule->command('two:cron')->timezone('Asia/Yangon')->runInBackground()->at("14:18");
+        $schedule->command('three:cron')->timezone('Asia/Yangon')->runInBackground()->at("16:19");
+        $schedule->command('four:cron')->timezone('Asia/Yangon')->runInBackground()->at("18:20");
     }
 
     /**
